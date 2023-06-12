09d73eda447cc8e8361af352e4644ac739c10dea
Paul Lewallen <paullewallencom@users.noreply.github.com>
2023-06-11 19:08:14 -0700

    Updated Files
    Updated files.

---
+++ ImageRequest/ImageRequest/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  ImageRequest
+//
+//  Created by Apple Computer on 6/11/23.
+//
+
+import UIKit
+
+enum KittenImageLocation: String {
+    case http = "http://www.kittenswhiskers.com/wp-content/uploads/sites/23/2014/02/Kitten-playing-with-yarn.jpg"
+    case https = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Kitten_in_Rizal_Park%2C_Manila.jpg/460px-Kitten_in_Rizal_Park%2C_Manila.jpg"
+    case error = "not a url"
+}
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imageLocation = KittenImageLocation.http.rawValue
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func handleLoadImageButtonPress(_ sender: Any) {
+        guard let imageUrl = URL(string: imageLocation) else {
+            print("Cannot create URL")
+            return
+        }
+        let task = URLSession.shared.dataTask(with: imageUrl) { (data, response, error) in
+            guard let data = data else {
+                print("no data, or there was an error")
+                return
+            }
+            let downloadedImage = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.imageView.image = downloadedImage
+            }
+        }
+        task.resume()
+        
+    }
+}
